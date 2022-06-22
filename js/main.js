073a13cd6e61e9c0feefebb5ec5aa2480a2b8a46
Yoonju Ryu <uuj92@naver.com>
2022-06-22 20:12:13 +0900

    22-06-22

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 //DOM
-
+const startBtn = document.querySelector(".startBtn");
+const startText = document.querySelector(".start");
 const game = document.querySelector("#game");
+let player = null;
 
 //Setting
-let playerMove = {
-  left: 0,
-};
-let moving = 0;
+let left = 237; //기준점
+let move = 5; //이동거리
+let browser_w = game.offsetWidth; // 플레이그라운드 너비
+let player_w = null; // 비행기 너비
+let invader_max = 10;
+let invader = [];
 
 // --------------------------------------- function ---------------------------------------
-function init() {
-  draw();
-}
-init();
+
+startBtn.addEventListener("initialize", function () {
+  player = document.querySelector(".player");
+  player_w = player.width;
+});
+
+
 //비행기 생성
 function draw() {
   const playerImg = document.createElement("img");
@@ -21,24 +28,40 @@ function draw() {
   game.appendChild(playerImg);
 }
 
-// 비행기 움직임
-let player = document.querySelector(".player");
-
-let left = 0; //기준점
-let move = 5; //이동거리
-let browser_w = game.offsetWidth;
-document.onkeydown = onkey_press;
-
-
-
-function moving() {
-    left += move;
-    requestAnimationFrame(moving);
+//첫 플레이 버튼
+function start() {
+  if (startText.style.display !== "none") {
+    startText.style.display = "none";
+    game.style.display = "block";
+  } else {
+    startText.style.display = "block";
+  }
+  draw();
+  let event = new CustomEvent("initialize");
+  startBtn.dispatchEvent(event);
 }
+
+
+//적군 비행기 생성
+function draw_p() {
+  for (i = 0; i < invader_max; ++i) {
+    let img = new Image();
+    img.src = "../resource/images/invader.png";
+    img.x = Math.random() * game.width;
+    img.y = Math.random() * game.height;
+    img.size = Math.sqrt(Math.random() * 100) + 1;
+    invader.push(img);
+  }
+}
+
+
+// 비행기 움직임
+document.onkeydown = onkey_press;
+let scope_w = browser_w - player_w;
 
 function onkey_press() {
   if (event.keyCode == 37) {
-    moving()
+    left = left - move;
     player.style.left = left + "px";
     if (left < 10) {
       left = 10;
@@ -46,8 +69,8 @@ function onkey_press() {
   } else if (event.keyCode == 39) {
     left = left + move;
     player.style.left = left + "px";
-    if (left > 500) {
-      left =  471;
+    if (left > scope_w) {
+      left = scope_w;
     }
   }
 }
