d186c8d29f80b90240d48da23911bd154ea57138
ryuworld <uuj92@naver.com>
2022-06-24 18:21:18 +0900

    fix

--- js/main.js
+++ js/main.js
@@ -6,12 +6,11 @@ const game = document.querySelector("#game");
 
 //Setting
 let left = 237; //기준점
-let move = 5; //이동거리
-let browser_w = game.offsetWidth; // 플레이그라운드 너비
+let move = 3; //이동거리
 let invader_max = 10;
 let invader = [];
 let keyCodeMap = [];
-let player_w = null;
+let missile = [];
 
 // --------------------------------------- function ---------------------------------------
 startBtn.addEventListener("click", start);
@@ -22,16 +21,29 @@ startBtn.addEventListener("initialize", initialize); //이거의 역할?
 function drawPlayer() {
   const playerImg = new Image(40);
   playerImg.src = "../resource/images/player.png";
-  player_w = playerImg.width;
   playerImg.classList.add("player");
   game.appendChild(playerImg);
 }
 
+//외계인생성
+function drawAlien() {
+  for (i = 0; i < invader_max; ++i) {
+    let alienImg = new Image(20);
+    alienImg.src = "../resource/images/invader.png";
+    invader.push(alienImg);
+    game.appendChild(alienImg);
+    alienImg.style.position = "absolute";
+    alienImg.style.left = `${Math.random() * game.offsetWidth}px`;
+    alienImg.style.top = `${Math.random() * game.offsetHeight / 3}px`; // es6문법
+  }
+}
+
 function initialize() {
+  let browser_w = game.offsetWidth; // 플레이그라운드 너비
   let player = document.querySelector(".player");
   let fream = {
     player,
-    scope_w : browser_w - player.width, //플레이어 너비 안구해짐
+    scope_w: browser_w - player.width,
   }
   window.requestAnimationFrame(update(fream)); // 최초 실행을 위해 근데 왜 굳이 requestAnimationFrame를 쓰나? time이 중요해서?
 }
@@ -50,6 +62,23 @@ function start() {
   startBtn.dispatchEvent(event);
 }
 
+//총알
+function drawBullet() {
+  let bulletImg = new Image();
+  bulletImg.src = "../resource/images/invader.png";
+  missile.push(bulletImg);  
+  game.appendChild(bulletImg);
+  setInterval(drawBullet,100)
+}
+
+//총알 이동
+function bulletMove() {
+  for (bullet of missile) {
+    bullet.style.top = 
+  }
+  
+}
+
 function update(fream) {
   return () => {
     onkey_press(fream.player, fream.scope_w);
@@ -58,26 +87,11 @@ function update(fream) {
 }
 
 
-//적군 비행기 생성
-function drawAlien() {
-  for (i = 0; i < invader_max; ++i) {
-    let img = new Image(20);
-    img.src = "../resource/images/invader.png";
-    invader.push(img);
-    game.appendChild(img);
-    img.style.position = "absolute";
-    img.style.left = `${Math.random() * game.offsetWidth}px`;
-    img.style.top = `${Math.random() * game.offsetHeight / 3}px`; // es6문법
-  }
-}
-
-
 // 비행기 움직임
 document.onkeydown = (e) => { keyCodeMap[e.keyCode] = true; }
-document.onkeyup =  (e) => { keyCodeMap[e.keyCode] = false; }
+document.onkeyup = (e) => { keyCodeMap[e.keyCode] = false; }
 
-
-function onkey_press(player, scope_w) {
+    function onkey_press(player, scope_w,bullet) {
   if (keyCodeMap[37]) {
     left = left - move;
     if (left < 0) {
@@ -85,10 +99,11 @@ function onkey_press(player, scope_w) {
     }
   } else if (keyCodeMap[39]) {
     left = left + move;
-    player.style.left = left + "px";
     if (left > scope_w) {
       left = scope_w;
     }
+  } else if (keyCodeMap[90]) {
+    console.log(bullet)
   }
-  player.style.left = left + "px";
+      player.style.left = left + "px";
 }
